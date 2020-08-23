86520fdb2a054e050ea59323eaedaa852676d671
NilE <nilenon@gmail.com>
2020-08-23 20:11:14 +0700

    [Add] AddDepartment route

--- server/Controller/Admin.js
+++ server/Controller/Admin.js
@@ -1,8 +1,11 @@
 const CryptoJS = require('crypto-js');
 const config = require('../config.json')
 const pool = require('../config/BorrowSystemDB');
-const { addAdmin } = require('../Model/Admin')
+const { addAdmin, addItemDepartment, addUserDepartment } = require('../Model/Admin')
 const printlog = require('../config/logColor');
+const UserModel = require('../Model/User');
+const { checkUserRole } = require('../Utilities/checkUserRole')
+const user = new UserModel()
 exports.addAdmin = async (req, res, next) => {
   try {
     var cipherPassword = CryptoJS.AES
@@ -10,6 +13,35 @@ exports.addAdmin = async (req, res, next) => {
       .toString();
     await addAdmin(req.body.userId, req.body.firstname, req.body.lastname, cipherPassword)
     res.status(200).json({ result: 'success', msg: 'Add Admin Success' });
+  } catch (err) {
+    printlog(
+      'Red',
+      `Add Admin Fail`
+    );
+    console.log(err);
+    res.status(500).json({ result: 'false', msg: err });
+  }
+};
+
+exports.addDepartment = async (req, res, next) => {
+  try {
+    const { userId, firstName, lastName, password, departmentName, departmentTelNo, departmentEmail,
+      placeBuilding, placeFloor, placeRoom
+    } = req.body
+    const role = await user.getUserRole(res.locals.authData.user[0].userId)
+    const userRole = await checkUserRole(role)
+    console.log(userRole)
+    if (userRole.admin === true) {
+      var cipherPassword = CryptoJS.AES
+        .encrypt(password, config.CRYPTO_SECRET_KEY)
+        .toString();
+
+      const addDepartment = await addItemDepartment(departmentName, departmentTelNo, departmentEmail, placeBuilding, placeFloor, placeRoom)
+      await addUserDepartment(userId, firstName, lastName, cipherPassword, addDepartment)
+    } else {
+      res.status(500).json({ result: 'false', msg: 'Permission deny' });
+    }
+
   } catch (err) {
     printlog(
       'Red',
